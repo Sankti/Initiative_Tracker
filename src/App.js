719e915c50e96982129cfe1e7fdea29a359bd735
Adam <57396704+Sankti@users.noreply.github.com>
2021-03-01 19:26:00 +0100

    Setting up character list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import './App.css';
 
+const playerCharacters = [
+  {
+    id: 0,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/0.png"
+  },
+  {
+    id: 1,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/1.png"
+  },
+  {
+    id: 2,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/2.png"
+  },
+  {
+    id: 3,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/3.png"
+  },
+  {
+    id: 4,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/4.png"
+  },
+  {
+    id: 5,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/5.png"
+  },
+  {
+    id: 6,
+    name: "Mistrz Podziemi",
+    thumb: "./assets/6.png"
+  },
+]
+
 function App() {
   return (
     <div className="app">
